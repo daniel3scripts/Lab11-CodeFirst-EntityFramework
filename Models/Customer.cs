858658ef481b3ac6d49f0cf03191f2341bdcedfa
daniel3scripts <paul.lugo@tecsup.edu.pe>
2022-11-09 00:00:27 -0500

    commit1

---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstDemo.Models
+{
+    public class Customer
+
+    {
+        public int CustomerID { get; set; } 
+        public string CustomerName { get; set; }   
+        
+        public string CustomerLastName { get; set; }
+        public string Dni { get; set; }
+        public string Email { get; set; }   
+        public string PhoneNumber { get; set; } 
+        public DateTime CreatedDate { get; set; }
+
+        
+    }
+}
---
+++ Models/Seller.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirstDemo.Models
+{
+    public class Seller
+    {
+
+        public int SellerId { get; set; }
+        public string SellerName { get; set; }
+
+        public string SellerLastName { get; set; }
+        public string Dni { get; set; } 
+        public string Email { get; set; }
+        public string Ruc { get; set; } 
+        public DateTime SellerDate { get; set; }
+       
+
+       
+
+
+    }
+}
